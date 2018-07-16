822cc3fbace3b974b650e09f88b2a6b6ab3f6ad0
Glitch (citrine-sheep) <none>
2018-07-16 07:04:06 +0000

    Import of freeCodeCamp/boilerplate-bcrypt

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
-// server.js
-// where your node app starts
+'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const app         = express();
 
-// init project
-const express = require('express')
-const app = express()
+fccTesting(app); //For FCC testing purposes
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
-})
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
 
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
+//END_ASYNC
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`)
-})
+//START_SYNC
+
+
+
+//END_SYNC
+
+
+app.listen(process.env.PORT || 3000, () => {});
