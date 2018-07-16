7be9c7c1c7f13211ba34415adee3863140da6ef7
Glitch (fcc-bcrypt-lessons-tm) <none>
2018-07-16 07:14:03 +0000

    🚣🙅 Checkpoint ./package.json:590980/1541 ./server.js:590980/633

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt = require('bcrypt');
 
 fccTesting(app); //For FCC testing purposes
 
@@ -12,7 +13,16 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, 13, (err, hash) => {
+  console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    if (err) console.log(err);
+      console.log(res); //true
+  });
+  // bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+  //     console.log(res); //false
+  // });
+});
 
 
 //END_ASYNC
