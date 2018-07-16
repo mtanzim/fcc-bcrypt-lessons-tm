c3697450d7dc27535c14057bb3951010f6683aa8
Glitch (fcc-bcrypt-lessons-tm) <none>
2018-07-16 07:25:03 +0000

    🗼🗼 Checkpoint ./server.js:590980/576

--- server.js
+++ server.js
@@ -13,23 +13,22 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash(myPlaintextPassword, 13, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     if (err) console.log(err);
-      console.log(res); //true
+    console.log(res); //true
   });
-  // bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
-  //     console.log(res); //false
-  // });
 });
 
 
 //END_ASYNC
 
 //START_SYNC
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+var result=bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
